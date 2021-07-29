913cf372b31e4fb0c4fe7d3f8a8d1642be2bc20c
Wang <ewang@Hunter.com>
2021-07-29 14:14:45 -0500

    Fixing data

--- src/prepare.py
+++ src/prepare.py
@@ -8,8 +8,10 @@ FOLDERS_TO_LABELS = {"n03445777": "golf ball", "n03888257": "parachute"}
 def get_files_and_labels(source_path):
     images = []
     labels = []
+    print(source_path)
     for image_path in source_path.rglob("*/*.JPEG"):
         filename = image_path.absolute()
+        print(filename)
         folder = image_path.parent.name
         if folder in FOLDERS_TO_LABELS:
             images.append(filename)
@@ -30,6 +32,8 @@ def main(repo_path):
     test_path = data_path / "raw/val"
     train_files, train_labels = get_files_and_labels(train_path)
     test_files, test_labels = get_files_and_labels(test_path)
+    print(test_files)
+    print(test_labels)
     prepared = data_path / "prepared"
     save_as_csv(train_files, train_labels, prepared / "train.csv")
     save_as_csv(test_files, test_labels, prepared / "test.csv")
